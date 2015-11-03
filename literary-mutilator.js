284c6d2d5092c3f3540221c705abb189d0800b99
Louis Gill <lgill7@cnm.edu>
2015-11-03 10:44:59 -0700

    1st paragraph

--- literary-mutilator.js
+++ literary-mutilator.js
@@ -2,5 +2,8 @@
  *changes the text color on a click event
  **/
 function clickEvent() {
-	document.getElementById("click").style.backgroundColor = blue;
+	document.getElementById("click").style.backgroundColor = "blue";
 }
+/**
+ *changes the background color when moused over
+ **/
