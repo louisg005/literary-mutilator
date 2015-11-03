1d3fb7e1a155b13296b0f36cb8752f15e7dfbb56
Louis Gill <lgill7@cnm.edu>
2015-11-03 10:33:08 -0700

    1st paragraph

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Literary Mutilator</title>
+		<script src="literary-mutilator.js" type="text/javascript"></script>
+	</head>
+	<body>
+	<p id="click" onclick="clickEvent();">Bacon ipsum dolor amet turkey spare ribs ham, landjaeger kielbasa jerky ground round ham hock pork loin biltong kevin beef ribs turducken bresaola pancetta. Filet mignon chicken shankle, chuck capicola short ribs t-bone beef ribs. Ham hock pork belly bacon leberkas. Bresaola hamburger tri-tip, cow pork loin shank pork belly jerky meatball chicken beef kielbasa tenderloin biltong.</p>
+	<p>Sausage pastrami drumstick t-bone frankfurter cupim brisket, shankle fatback pork belly sirloin ribeye short ribs turkey. Turkey cow jerky frankfurter prosciutto pork chop. Cow tri-tip jowl alcatra short loin landjaeger. Salami venison capicola meatloaf prosciutto. Sausage landjaeger brisket bacon. Swine fatback cow bresaola pork loin tail shankle filet mignon pancetta spare ribs rump meatball. Ground round kielbasa corned beef short loin jowl pig t-bone jerky chicken leberkas.</p>
+	<p>Chicken kielbasa turducken kevin. Pancetta shank ribeye pork chop pig biltong. Tail meatball pork chop prosciutto capicola. Drumstick bresaola ball tip sausage, pastrami turducken shoulder ham meatball shank strip steak flank. Pork loin porchetta sausage brisket. Bacon pastrami pig beef ball tip turkey shank turducken alcatra porchetta bresaola short ribs drumstick cow. Pancetta tri-tip boudin sirloin kevin ham hock landjaeger filet mignon pork frankfurter tongue beef ribs porchetta biltong.</p>
+	</body>
+</html>
---
+++ literary-mutilator.js
@@ -0,0 +1,6 @@
+/**
+ *changes the text color on a click event
+ **/
+function clickEvent() {
+	document.getElementById("click").style.color = red;
+}
